ba8c86f97765ea47feb382d66f16448527c3a8f1
kamran khorasani <kamran.gamedev@gmail.com>
2022-10-10 09:41:11 +0330

    - add hello world page - add zebble logo - unused layouts removed

---
+++ HelloWord/ViewModel/StartUp.cs
@@ -0,0 +1,22 @@
+﻿namespace ViewModel
+{
+	using System.Threading.Tasks;
+	using Zebble;
+	using Zebble.Mvvm;
+	using Olive;
+    using Domain;
+
+    class StartUp
+	{
+		public static Task Run()
+		{
+			// Comment this line out to use the Live API
+			Api.Fake();
+
+
+			ViewModel.Go<LandingPage>(PageTransition.Fade);
+
+			return Task.CompletedTask;
+		}
+	}
+}
