e016fbe821013fcb8d6a22881d3af4448c115c0c
kamran khorasani <kamran.gamedev@gmail.com>
2022-10-10 19:29:07 +0330

    Second Task Done

---
+++ ECardSample/App.UI/Views/Pages/Home.zbl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Zebble;
+
+namespace UI.Pages
+{
+    partial class Home
+    {
+        public override async Task OnInitializing()
+        {
+            await base.OnInitializing();
+        }
+
+        public async Task ChangeThemeBtnTapped()
+        {
+            Zebble.Device.Screen.DarkMode = !Zebble.Device.Screen.DarkMode;
+            await UIContext.RefreshUI();
+        }
+    }
+}
---
+++ ECardSample/update-image-sizes.bat
@@ -0,0 +1,1 @@
+zebble-image update-sizes
